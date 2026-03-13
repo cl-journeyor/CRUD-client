179db2f6ca742a8abff89b58475a7892ce6b0b2c
cl-journeyor <genericobject711@yahoo.com>
2026-03-12 19:02:02 -0700

    DONE: HTML forms as db rows

--- program.cljs
+++ program.cljs
@@ -1,3 +1,29 @@
+;;;; UTILITIES
+
+(defn get-elem
+  [id]
+  (.getElementById js/document id))
+
+(defn new-elem
+  ([name]
+   (.createElement js/document name))
+  ([name txt]
+   (let [elem (new-elem name)]
+     (set! (.-textContent elem) txt)
+     elem)))
+
+(defn new-hidden
+  [name value]
+  (let [hidden (new-elem "input")]
+    (set! (.-type hidden) "hidden")
+    (set! (.-name hidden) name)
+    (set! (.-value hidden) value)
+    hidden))
+
+;;;; ----------
+
+;;;; DOMAIN CODE
+
 (defn get-page-number
   []
   (let [unsafe-page-number (-> js/location
@@ -9,26 +35,47 @@
       1
       unsafe-page-number)))
 
-(defn fetch-row-page-async
-  [page]
-  (-> (js/fetch (str "http://localhost:5070/?page=" page))
-      (.then #(.json %))))
+(defn render-employees!
+  [emps]
+  (letfn [(new-prop-cell
+            [name value]
+            (doto (new-elem "div" value)
+              (.appendChild (new-hidden name value))))]
+    (doseq [emp emps]
+      (let [delete-btn (let [btn (new-elem "button" "Delete")]
+                         (set! (.-type btn) "button")
+                         (.addEventListener
+                          btn
+                          "click"
+                          #(println (.-id emp)))
+                         btn)
+            cells [(new-prop-cell "id" (.-id emp))
+                   (new-prop-cell "name" (.-name emp))
+                   (new-prop-cell "role" (.-role emp))
+                   (new-prop-cell "salary" (.-salary emp))
+                   (new-prop-cell "added-date" (.-added_date emp))
+                   (doto (new-elem "div")
+                     (.appendChild (new-elem "button" "Update")))
+                   (doto (new-elem "div")
+                     (.appendChild delete-btn))]
+            row (let [div (new-elem "div")]
+                  (set! (.-className div) "employee-row")
+                  div)]
+        (doseq [cell cells]
+          (.appendChild row cell))
+        (.appendChild (get-elem "body") row)))))
 
-(defn new-elem
-  ([name]
-   (.createElement js/document name))
-  ([name txt]
-   (let [elem (new-elem name)]
-     (set! (.-textContent elem) txt)
-     elem)))
+(defn render-error!
+  [msg]
+  (.appendChild (get-elem "body") (new-elem "h1" msg)))
 
-(defn get-elem
-  [id]
-  (.getElementById js/document id))
+(defn load-app!
+  []
+  (-> (js/fetch (str "http://localhost:5070/?page=" (get-page-number)))
+      (.then #(.json %))
+      (.then #(if (instance? js/Object %)
+                (render-employees! (.-employees %))
+                (render-error! %)))
+      (.catch render-error!)))
 
-
-
-
-;;; Example code.
-(-> (.resolve js/Promise (fetch-row-page-async (get-page-number)))
-    (.then println))
+(load-app!)
